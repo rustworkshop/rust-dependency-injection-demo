1386e56cb87c94afebc868a12ff71f7ed8bdc903
Tim Abell <tim@timwise.co.uk>
2023-05-03 12:08:00 +0100

    Refactor - introduce traits and structs for dependency
    Use async-trait crate to allow us to make traits with async fn's. Not
    allowed by default. https://www.reddit.com/r/rust/comments/fe9nbs/comment/fjmp2im/
    
    https://crates.io/crates/async-trait
    
    `cargo add async-trait`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use async_trait::async_trait;
 use reqwest::Error;
 use serde::Deserialize;
 
@@ -6,23 +7,54 @@ struct Repo {
     stargazers_count: u32,
 }
 
+// main cli that uses the business logic and will need to inject the real dependency
 #[tokio::main] // https://docs.rs/tokio/latest/tokio/index.html
 async fn main() -> Result<(), Error> {
     const REPO_PATH: &str = "timabell/gitopolis";
 
-    let repo_analysis = analyse_repo(REPO_PATH).await;
+    // set up business logic
+    let analyser = RepoAnalyser {
+        repo_reader: Box::new(RealRepoReader {}), // inject the real implementation of RepoReader in to the analyser
+    };
 
-    println!("{}", repo_analysis?);
+    let repo_analysis = analyser.analyse_repo(REPO_PATH).await?; // run the business logic
+
+    println!("{}", repo_analysis);
     Ok(())
 }
 
-async fn analyse_repo(repo: &str) -> Result<String, Error> {
-    let stars = get_stars(repo).await?;
-    let repo_analysis = format!("{} has {} stars", repo, stars);
-    Ok(repo_analysis)
+// business logic struct
+struct RepoAnalyser {
+    repo_reader: Box<dyn RepoReader>, // here is the pointer to whatever was injected
 }
 
-async fn get_stars(repo: &str) -> Result<u32, Error> {
+// implementation of business logic, that has a dependency to inject
+impl RepoAnalyser {
+    async fn analyse_repo(&self, repo: &str) -> Result<String, Error> {
+        let stars = &self.repo_reader.get_stars(repo).await?; // <== use the injected dependency
+        let repo_analysis = format!("{} has {} stars", repo, stars); // some "business logic"
+        Ok(repo_analysis)
+    }
+}
+
+// the dependency, defined as a trait saying what we need it to do. Like an interface in C#.
+#[async_trait]
+trait RepoReader {
+    async fn get_stars(&self, repo: &str) -> Result<u32, Error>; // we need something that does this
+}
+
+// Empty struct to hang real logic off. Could add state/data here later if needed
+struct RealRepoReader {}
+
+// Real implementation of dependency logic that calls out to internet
+#[async_trait]
+impl RepoReader for RealRepoReader {
+    async fn get_stars(&self, repo: &str) -> Result<u32, Error> {
+        get_stars_async(repo).await
+    }
+}
+
+async fn get_stars_async(repo: &str) -> Result<u32, Error> {
     // based on https://rust-lang-nursery.github.io/rust-cookbook/web/clients/apis.html
 
     // https://docs.rs/reqwest/latest/reqwest/struct.ClientBuilder.html#method.user_agent
@@ -39,3 +71,5 @@ async fn get_stars(repo: &str) -> Result<u32, Error> {
         serde_json::from_str(json_string.as_str()).expect("JSON was not well-formatted");
     Ok(repo.stargazers_count)
 }
+
+// todo: a test that injects a mock
